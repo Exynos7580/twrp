6e204aeaee5eb9a3e2d5fb4af02131951613562e
Stricted <info@stricted.net>
2018-01-30 21:28:23 +0100

    remove unneeded init library
    Change-Id: I88478fa66cadbb921a51afc49ddd58b8af6653b8

--- a3xelte/BoardConfig.mk
+++ a3xelte/BoardConfig.mk
@@ -20,8 +20,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_INIT_VENDOR_LIB := libinit_a3xelte
-
 # Kernel
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := lineageos_a3xelte_defconfig
